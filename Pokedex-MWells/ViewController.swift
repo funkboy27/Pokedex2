e195e58e9bc5dff1c63a6c25cb7e56134801e0ea
Matt Wells <mattwells24@hotmail.com>
2016-07-18 15:08:25 -0700

    Initial Commit

---
+++ Pokedex-MWells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-MWells
+//
+//  Created by Matthew Wells on 2016-07-18.
+//  Copyright © 2016 Matthew Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
